fc3c5d865f3e0ca160cb721d1e5c167859cf53a2
Miguel Coronel <m.coronel.seg@gmail.com>
2015-12-07 16:41:43 -0600

    List Items

---
+++ platforms/android/assets/www/js/vc/index.js
@@ -0,0 +1,23 @@
+(function () {
+
+  $(document).on("deviceready", function (e) {
+    e.preventDefault();
+
+    if (localStorage.items) {
+      var items = JSON.parse(localStorage.items);
+    } else {
+      var items = [];
+    }
+
+    for (i = 0; i < items.length; i++) {
+      if (items[i]) {
+        $("<li class='ui-li-has-thumb'>" +
+          "<a href='#' class='ui-btn waves-effect waves-button waves-effect waves-button'>" +
+            "<img src='"+ items[i].photo +"' style='min-width:80px;min-height:80px;' class='ui-thumbnail ui-thumbnail-circular'>" +
+            "<h2>"+ items[i].name + "</h2>" +
+            "<p><b>Precio: </b>"+ items[i].price + "</p>" +
+          "</a></li>").appendTo("#items");
+      }
+    }
+  });
+})();
